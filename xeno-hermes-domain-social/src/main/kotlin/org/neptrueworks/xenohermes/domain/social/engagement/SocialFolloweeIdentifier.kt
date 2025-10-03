226034050dc230a439d0c2b68bb84cc78688b968
Neptrue Lin <Neptrue_Lin@outlook.com>
2025-10-03 12:01:49 +0800

    feat(domain): Remove unused Follower and Followee

--- xeno-hermes-domain-social/src/main/kotlin/org/neptrueworks/xenohermes/domain/social/engagement/SocialFolloweeIdentifier.kt
+++
@@ -1,7 +0,0 @@
-package org.neptrueworks.xenohermes.domain.social.engagement
-
-import org.neptrueworks.xenohermes.domain.common.models.InlineClass
-
-@InlineClass
-public data class SocialFolloweeIdentifier(val identifier: Long)
-
--- xeno-hermes-domain-social/src/main/kotlin/org/neptrueworks/xenohermes/domain/social/engagement/SocialFollowerIdentifier.kt
+++
@@ -1,6 +0,0 @@
-package org.neptrueworks.xenohermes.domain.social.engagement
-
-import org.neptrueworks.xenohermes.domain.common.models.InlineClass
-
-@InlineClass
-public data class SocialFollowerIdentifier(val identifier: Long)
